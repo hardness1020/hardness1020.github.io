2301578c829f66e7e1c24e08ba09773bdc54303f
root <root@DESKTOP-G7DB14Q.localdomain>
2020-10-10 01:41:38 +0800

    web

---
+++ precache-manifest.4381008cd28994bde926390236cb82a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1530289d63ebf39944162db51e3be70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "910a77163e9e2eee6d57",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910a77163e9e2eee6d57",
+    "url": "/static/js/main.9882020c.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
